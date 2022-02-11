5f8c341feea806fd671749ededb1d051384d7494
averenceanu <alexandra.verenceanu@mail.mcgill.ca>
2022-02-10 22:24:08 -0500

    change app title and icon

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,34 @@
 import React from "react";
+import './navbar.scss'
 
-
-export default function Navbar(props) {
+export default function Navbar (props) {
   return (
-      
-  <div>
-    <p>HELLO IM HERE</p>
-  </div>
+    <nav className="navbar navbar-expand-lg fixed-top navbar-light bg-light ">
+      <a className="navbar-brand logo-size" href="#">
+      <img src="./images/logo.png" alt="logo" width="30%" height="30%"/>
+      </a>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Covid Test / Vaccination</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Covid Statistics</a>
+          </li>
+          <li class="nav-item active">
+            <a type="button" href="#" class="button-18">Login</a>
+          </li>
+          <li class="nav-item active">
+            <a type="button" href="#" class="button-18">Sing-up</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
+
+
+ 

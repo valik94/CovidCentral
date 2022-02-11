1d04cf4120801055415fe02257b40e17be04f35c
Valentin <valentin.lyash@gmail.com>
2022-02-11 17:25:45 -0500

    restructure db  to fit project ERD, medical records projects

---
+++ server/db/seeds/seeds/02_patients.sql
@@ -0,0 +1,7 @@
+INSERT INTO patients (first_name, last_Name, email, healthcare_card, phone, emergency_contact, gender, date_of_birth, practitioner_id)
+VALUES
+('Clinton','Schroeder','phasellus.at@hotmail.ca', 'DB354', '(714) 365-6546', '547-438-0720', 'male', '2003-01-17', 1),
+('Carlos','Salas','ultricies.adipiscing@hotmail.edu', 'DC418', '120-353-1663', '177-879-3534', 'male', '1987-03-20', 2),
+('Octavia','Armstrong','auctor.nunc.nulla@aol.org', 'DT231', '(477) 425-7844', '454-528-1011', 'female', '1993-10-20', 3),
+('Samson','Ruiz','rhoncus.proin@yahoo.com', 'AT480', '(742) 579-6910', '(816) 816-2233', 'male', '1994-07-19', 4),
+('Jason','Hooper','fringilla.est@yahoo.ca', 'RO194', '(811) 639-8867', '631-775-9627', 'male', '1982-07-10', 5);

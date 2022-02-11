c19d035c986a6d8701988638f874a0613083c192
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-02-11 01:55:03 -0500

    install bycrypt, replace var to const in app.js for server

--- server/app.js
+++ server/app.js
@@ -1,14 +1,15 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bcrypt = require("bcryptjs");
 
 const db = require('./configs/db.config');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());

53c7c879af33aa36da68d0b25830074d45183f9e
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-02-12 13:04:30 -0500

    delete index.html

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
-import './index.scss'
-import Navbar from './components/Navbar';
-import NavCarousel from './components/NavCarousel';
-import Form from './components/Form';
+import "./index.scss";
+import Navbar from "./components/Navbar";
+import NavCarousel from "./components/NavCarousel";
+import Form from "./components/Form";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-        <Navbar/>
-        <NavCarousel/>
-        <Form/>
+      <Navbar />
+      <NavCarousel />
+      <Form />
     </div>
   );
 }

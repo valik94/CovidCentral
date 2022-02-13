8048085d72eec49ff2f7eaf3842907d6fd6051fe
averenceanu <alexandra.verenceanu@mail.mcgill.ca>
2022-02-13 17:56:42 -0500

    change in appointments schema and new seeds

--- server/db/seeds/seeds/05_appointments.sql
+++ server/db/seeds/seeds/05_appointments.sql
@@ -1,6 +1,21 @@
-INSERT INTO appointments (description, start_time, notification_sent, patient_id, practitioner_id)
-VALUES ('medication effectivness', '2022-10-10 12:00:00.000', true, 1, 1),
-('prescribtion', '2022-10-10 13:30:00.000', false, 2, 2),
-('follow up', '2022-04-15 15:00:00.000', true, 3, 3),
-('consultation', '2022-08-09 10:00:00.000', false, 4, 4),
-('follow up','2022-03-08 09:30:00.000', true, 5, 5)
+INSERT INTO appointments 
+(startAt, endAt, summary, color, notification_sent, patient_id, practitioner_id)
+VALUES 
+('2022-10-13 12:00:00.000', '2022-10-13 13:00:00.000', 'medication effectivness', 'blue', false, 1, 1),
+('2022-10-13 14:00:00.000', '2022-10-13 15:00:00.000', 'Reassess Insulin prescription', 'blue', false, 2, 1),
+('2022-10-13 16:00:00.000', '2022-10-13 17:00:00.000', 'Follow-up blood results', 'blue', true, 3, 1),
+('2022-10-14 12:00:00.000', '2022-10-14 13:00:00.000', 'Medication change', 'blue', false, 2, 1),
+('2022-10-14 14:00:00.000', '2022-10-14 15:00:00.000', 'Follow-up CT results', 'blue', false, 1, 1),
+('2022-10-14 11:00:00.000', '2022-10-14 11:30:00.000', 'Diabets teaching', 'blue', false, 3, 1),
+('2022-10-14 16:00:00.000', '2022-10-14 17:00:00.000', 'Follow-up MRI results', 'blue', false, 4, 1),
+('2022-10-15 10:00:00.000', '2022-10-15 11:00:00.000', 'Teaching on Metropolol', 'blue', true, 1, 1),
+('2022-10-15 12:00:00.000', '2022-10-15 13:00:00.000', 'Annual General Checkup', 'blue', false, 2, 1),
+('2022-10-15 14:00:00.000', '2022-10-15 15:00:00.000', 'Emergency consultation', 'blue', true, 1, 1),
+('2022-10-15 17:00:00.000', '2022-10-15 18:00:00.000', 'Consultation: heart palpitations', 'blue', false, 1, 1),
+('2022-10-16 12:00:00.000', '2022-10-16 13:00:00.000', 'Follow-up change in medication', 'blue', false, 3, 1),
+('2022-10-16 09:00:00.000', '2022-10-16 10:00:00.000', 'Annual General Checkup', 'blue', true, 4, 1),
+('2022-10-16 12:00:00.000', '2022-10-16 13:00:00.000', 'Follow-up heart echo', 'blue', false, 1, 1),
+('2022-10-16 15:00:00.000', '2022-10-16 16:00:00.000', 'Medication change: digoxin', 'blue', true, 4, 1),
+('2022-10-17 14:00:00.000', '2022-10-17 14:30:00.000', 'Annual General Checkup', 'blue', false, 2, 1),
+('2022-10-17 16:00:00.000', '2022-10-17 19:00:00.000', 'Follow-up post cardiac surgery', 'blue', false, 2, 1)
+

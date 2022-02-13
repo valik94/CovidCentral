8ff045a4baa889c92696c5ae829de98983a6421f
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-02-12 21:36:35 -0500

    upload w/error

--- server/routes/register.js
+++ server/routes/register.js
@@ -3,9 +3,20 @@ const router = express.Router();
 
 /* GET home page. */
 module.exports = (db) => {
-  router.post("/register", function (req, res, next) {
-    console.log("POST: we got here")
+  router.post("/", function (req, res, next) {
+    console.log("POST: we got here");
+    return res.send({})
     const {first_name, last_name, email, password, specialty} = req.body
+    // const first_name = req.body.first_name;
+    // const last_name = req.body.last_name;
+    // const email = req.body.email;
+    // const password = req.body.password;
+    // const specialty = req.body.specialty;
+const getUserWithEmail = (db, email) => {
+  return Promise.resolve({
+    rows: []
+  })
+}
     const addUser = function (user) {
       return db
         .query(
@@ -21,15 +32,13 @@ VALUES ($1, $2, $3, $4, $5) RETURNING *;`,
           console.log(err.message);
         });
     };
-    
-    
 
     getUserWithEmail(email)
       .then((result) => {
         if (result) {
           return result;
         } else {
-          return getUserWithEmail(email);
+          // return getUserWithEmail(email);
         }
       })
       .then((result) => {

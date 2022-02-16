eb8131908a55ac1c7da8ef47f8033552d7872cda
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-02-16 18:48:03 -0500

    update patient profile

--- client/src/components/patientsList/PatientsList.js
+++ client/src/components/patientsList/PatientsList.js
@@ -128,7 +128,6 @@ export default function PatientsList() {
               filteredPatient.map((patient) => (
                 <TableRow
                   key={patient.id}
-                
                   sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
                 >
                   <TableCell component="th" scope="row">

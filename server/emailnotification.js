736a52cfbfc86729d7951dd1a8c5270b3332ca35
Valentin <valentin.lyash@gmail.com>
2022-02-20 18:20:44 -0500

    adding email notification text newline and minor formatting of string

--- server/emailnotification.js
+++ server/emailnotification.js
@@ -6,19 +6,21 @@ const sendEmail = function(appointmentTo, patientObject){
   const {first_name, last_name} = patientObject
   console.log("appointmentTo inside sendMail is:", appointmentTo)
   console.log("patientObject is: ",patientObject)
-  // console.log(`process env SEND GRID API: `,process.env.SENDGRID_API_KEY)
 
   const startTime = (appointmentTo.startAt).toString().slice(0, 21)
   const EndTime = (appointmentTo.endAt).toString().slice(16, 21)
-  // const appointment_datetime = new Date(startDate)
   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
   const msg = {
     to: 'senseivalik@gmail.com', // Change to your recipient
     from: 'valentin.lyash@gmail.com', // Change to your verified sender
     subject: 'Sending with SendGrid is Fun',
     // text: 'and easy to do anywhere, even with Node.js',
-    html: `<strong>Hello Dear ${first_name} ${last_name},\n</strong> Please be aware that your appointment has been booked for <strong>${startTime}</strong> and end at <strong>${EndTime}</strong>.\n We look forward to seeing you then. For rescheduling or cancellations please give us a call.\n\n
-    Thank you and have a healthy day.\n
+    html: `<strong>Hello Dear ${first_name} ${last_name},
+    <br> </strong> Please be aware that your appointment has been booked for <strong>${startTime}</strong> and will end at <strong>${EndTime}</strong>.
+    <br> We look forward to seeing you then. For rescheduling or cancellations please give us a call.
+    <br> <br> 
+    Thank you and have a healthy day.
+    <br> 
     -Team dRecords`,
   }
   sgMail
--- server/routes/appointments.js
+++ server/routes/appointments.js
@@ -1,7 +1,6 @@
 //POST Appointments
 
-// const express = require('express');
-// const cookieSession = require('cookie-session');
+
 const router = require("express").Router();
 const {sendEmail} = require('../emailnotification')
 
@@ -11,18 +10,12 @@ const bookAppointments = async function( db, startAt, endAt, summary, color, not
 
     const appointmentTo = await db.query(`INSERT INTO appointments ("startAt", "endAt", summary, color, notification_sent, patient_id, practitioner_id)
     VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`, [startAt, endAt, summary, color, notification_sent, patient_id, practitioner_id]) //await makes this return sequentially. Meaning this returns first. await is similar in nature to .then()
-
-    // const appointments = db.query(`INSERT INTO appointments ("startAt", "endAt", summary, color, notification_sent, patient_id, practitioner_id)
-    // VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`, [startAt, endAt, summary, color, notification_sent, patient_id, practitioner_id])
+  
 
     const patientBasedOnId = await db.query(`SELECT * FROM patients WHERE patients.id = $1`, [patient_id]) // this returns second after appointmentTo returns. The await ensures we don't move forward before this is resolved.
 
-    // console.log(` patientBasedOnId ISSSSS`, patientBasedOnId)
-    // console.log(`APPOINTMENTS: `, appointments)
-    // promises.push(appointments)
-    // promises.push(patientBasedOnId)
-    return {appointmentTo , patientBasedOnId} //
-    // return promises
+    return {appointmentTo , patientBasedOnId} 
+   
   } catch (error) {
     console.log(error)
     throw new Error('error');
@@ -42,18 +35,7 @@ module.exports = (db) => {
       console.log(`ALL MY patientBasedOnId ARE:`, patientBasedOnId)
       sendEmail(appointmentTo.rows[0], patientBasedOnId.rows[0])
       res.json("successfully saved appointment!").end()
-      // Promise.all(allmyPromises)
-      // .then((result) =>{ //result is an array
-      //   console.log("RESULT::::: ", result[0])
-      //   console.log("Result[0].rows=====", result[0].rows)
-      //   const appointment = result[0].rows[0]
-      //   console.log(`appointment is ===`, appointment)
-      //   sendEmail(appointmentTo, patientBasedOnId)
-      //   res.json(appointmentTo.rows[0])
-      // })
-      // .catch((err) => {
-      //   res.status(500).json({ err: err.message });
-      // })
+
       
   })
   return router;

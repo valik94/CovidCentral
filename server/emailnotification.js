74aab83c538cfdb825b9abebecfcb2ac8496530f
Valentin <valentin.lyash@gmail.com>
2022-02-20 17:41:53 -0500

    fixing appointments route to async-await pattern and separation of concerns in order to debug properly the server errors. Due to asynchronous promises we were not able to control returned promises flow, so based on advise, I decided to make try-catch block with async-await pattern and object definitions outside the post route. Next, I passed in both query await results int emailnotification and destructured it to include patients name obtained from the second query

--- server/emailnotification.js
+++ server/emailnotification.js
@@ -2,11 +2,14 @@
 // https://github.com/sendgrid/sendgrid-nodejs
 // javascript
 const sgMail = require('@sendgrid/mail')
-const sendEmail = function(patient){
-  console.log("Patient inside sendMail is:", patient)
-  console.log(`process env SEND GRID API: `,process.env.SENDGRID_API_KEY)
-  const startTime = (patient.startAt).toString().slice(0, 21)
-  const EndTime = (patient.endAt).toString().slice(16, 21)
+const sendEmail = function(appointmentTo, patientObject){
+  const {first_name, last_name} = patientObject
+  console.log("appointmentTo inside sendMail is:", appointmentTo)
+  console.log("patientObject is: ",patientObject)
+  // console.log(`process env SEND GRID API: `,process.env.SENDGRID_API_KEY)
+
+  const startTime = (appointmentTo.startAt).toString().slice(0, 21)
+  const EndTime = (appointmentTo.endAt).toString().slice(16, 21)
   // const appointment_datetime = new Date(startDate)
   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
   const msg = {
@@ -14,7 +17,7 @@ const sendEmail = function(patient){
     from: 'valentin.lyash@gmail.com', // Change to your verified sender
     subject: 'Sending with SendGrid is Fun',
     // text: 'and easy to do anywhere, even with Node.js',
-    html: `<strong>Hello Dear Patient.name,\n</strong> Please be aware that your appointment has been booked for <strong>${startTime}</strong> and end at <strong>${EndTime}</strong>.\n We look forward to seeing you then. For rescheduling or cancellations please give us a call.\n\n
+    html: `<strong>Hello Dear ${first_name} ${last_name},\n</strong> Please be aware that your appointment has been booked for <strong>${startTime}</strong> and end at <strong>${EndTime}</strong>.\n We look forward to seeing you then. For rescheduling or cancellations please give us a call.\n\n
     Thank you and have a healthy day.\n
     -Team dRecords`,
   }
--- server/routes/appointments.js
+++ server/routes/appointments.js
@@ -5,41 +5,56 @@
 const router = require("express").Router();
 const {sendEmail} = require('../emailnotification')
 
+const bookAppointments = async function( db, startAt, endAt, summary, color, notification_sent, patient_id, practitioner_id) { //async defines the TYPE of function this is. Async specifically allows for the functionality of awaiting of each promise line being resolved before moving onto the next promise.
+  try {
+    const promises = [];
+
+    const appointmentTo = await db.query(`INSERT INTO appointments ("startAt", "endAt", summary, color, notification_sent, patient_id, practitioner_id)
+    VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`, [startAt, endAt, summary, color, notification_sent, patient_id, practitioner_id]) //await makes this return sequentially. Meaning this returns first. await is similar in nature to .then()
+
+    // const appointments = db.query(`INSERT INTO appointments ("startAt", "endAt", summary, color, notification_sent, patient_id, practitioner_id)
+    // VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`, [startAt, endAt, summary, color, notification_sent, patient_id, practitioner_id])
+
+    const patientBasedOnId = await db.query(`SELECT * FROM patients WHERE patients.id = $1`, [patient_id]) // this returns second after appointmentTo returns. The await ensures we don't move forward before this is resolved.
+
+    // console.log(` patientBasedOnId ISSSSS`, patientBasedOnId)
+    // console.log(`APPOINTMENTS: `, appointments)
+    // promises.push(appointments)
+    // promises.push(patientBasedOnId)
+    return {appointmentTo , patientBasedOnId} //
+    // return promises
+  } catch (error) {
+    console.log(error)
+    throw new Error('error');
+  }
+}
 
 //POST book Appointments page
 module.exports = (db) => {
 
-  router.post("/",(req,res) => {
-    const promises = [];
+  router.post("/", async (req,res) => { //this callback (req,res) is an async function
     const sessionId = req.session.user_id; //getting session using id being sent to client
     const { startAt, endAt, summary, color, notification_sent, patient_id } = req.body; //getting information from FORM that is sent from frontend
-
-    console.log("REQ BODY")
-    const bookAppointments = function( startAt, endAt, summary, color, notification_sent, patient_id, practitioner_id) {
-
-    console.log("REQ=======================:", req.body)
-
-      const appointments= db.query(`INSERT INTO appointments ("startAt", "endAt", summary, color, notification_sent, patient_id, practitioner_id)
-      VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`, [startAt, endAt, summary, color, notification_sent, patient_id, practitioner_id])
-
-      const patientBasedOnId = db.query(`SELECT * FROM patients WHERE patients.id = $1`, [patient_id])
-      console.log(` patientBasedOnId ISSSSS`, patientBasedOnId)
-      promises.push(appointments)
-      promises.push(patientBasedOnId)
-      Promise.all(promises)
-      .then((result) =>{
-        console.log(`THE RESULT IS OF APPOINTMENTS IS:`,result)
-        const appointment = result.rows[0]
-        console.log(`appointment is ===`, appointment)
-        sendEmail(appointment)
-        res.json(result.rows[0])
-      })
-      .catch((err) => {
-        res.status(500).json({ err: err.message });
-      })
-    }
-    bookAppointments(startAt, endAt, summary, color, notification_sent, patient_id, sessionId)
-
+    
+      console.log("WE ARE INSIDE POST ")
+      const {appointmentTo, patientBasedOnId} = await bookAppointments( db, startAt, endAt, summary, color, notification_sent, patient_id, sessionId) //calling function and destructuring the result into appointmentTo and patientBasedOnId
+      console.log(`ALL MY appointmentTo ARE: `, appointmentTo)
+      console.log(`ALL MY patientBasedOnId ARE:`, patientBasedOnId)
+      sendEmail(appointmentTo.rows[0], patientBasedOnId.rows[0])
+      res.json("successfully saved appointment!").end()
+      // Promise.all(allmyPromises)
+      // .then((result) =>{ //result is an array
+      //   console.log("RESULT::::: ", result[0])
+      //   console.log("Result[0].rows=====", result[0].rows)
+      //   const appointment = result[0].rows[0]
+      //   console.log(`appointment is ===`, appointment)
+      //   sendEmail(appointmentTo, patientBasedOnId)
+      //   res.json(appointmentTo.rows[0])
+      // })
+      // .catch((err) => {
+      //   res.status(500).json({ err: err.message });
+      // })
+      
   })
   return router;
 }
